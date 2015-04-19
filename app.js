a72284501d32f2d76accab3030b8c18d2438c75b
Chapman <chapman@apextion.com>
2015-04-19 15:51:44 -0400

    added heroku port listner

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.engine('jsx', require('express-react-views').createEngine({beautify:true}));
 
 app.get('/', require('./routes').index);
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('Oh, im there.')
 });
+
