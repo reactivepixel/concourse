9ecf770054d393f8d04a895bffce012898102a49
Chapman <chapman@apextion.com>
2015-04-20 19:05:33 -0400

    Addred multiple routes

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var app = express();
 app.set('view engine', 'jsx'); // Set the View Engine
 app.engine('jsx', require('express-react-views').createEngine({beautify:true}));
 
-app.get('/', require('./routes').index);
+
+app.route('/').get(require('./routes').index);
+app.route('/list').get(require('./routes').list);
 
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('Oh, im there.')
 });
-
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 exports.index = function(req, res){
-  res.render('index', { name: 'World' });
+  res.render('index', { name: 'World', page_title: 'SEO Time' });
+};
+
+exports.list = function(req, res){
+  res.render('list', { name: 'killer', page_title: 'We\'re gonna need a bigger boat' });
 };
