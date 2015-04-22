09e31e22d5ffab5cdcaf2a80af3a984f646d9170
Chapman <chapman@apextion.com>
2015-04-22 13:28:33 -0400

    stripping down layouts default prepping for bootstrap

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -3,10 +3,20 @@ var React = require('react');
 var DefaultLayout = React.createClass({
   render: function() {
     return (
-      <html>
-        <head><title>{this.props.page_title}</title></head>
-        <body>{this.props.children}</body>
-      </html>
+			<html>
+			<head>
+			  <meta charSet="utf-8" />
+			  <meta name="viewport" content="width=device-width, maximum-scale=1" />
+
+			  <title>Portfolio</title>
+			</head>
+			<body>
+			  <div id="main_content" className="container">
+			    
+			    <a href="#top" className="col-xs-12 hidden-sm hidden-md hidden-lg text-center">Back to Top</a>
+			  </div>
+			</body>
+			</html>
     );
   }
 });
