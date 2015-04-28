dd7bf5c299ea150c18882f3e4d32419975a31dd9
Chapman <chapman@apextion.com>
2015-04-28 01:45:18 -0400

    Basic Mongo save and find working through mongoose

--- app.js
+++ app.js
@@ -1,13 +1,66 @@
+// npm modules
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+// config vars
+var routes = require('./routes');
+
+// View Rendering with React
 app.set('view engine', 'jsx'); // Set the View Engine
 app.engine('jsx', require('express-react-views').createEngine({beautify:true}));
 
+// Routes
+app.route('/').get(routes.index);
+app.route('/list').get(routes.list);
 
-app.route('/').get(require('./routes').index);
-app.route('/list').get(require('./routes').list);
 
-var server = app.listen(process.env.PORT || 3000, function(){
-	console.log('Oh, im there.')
+// Disable etag headers on responses
+app.disable('etag');
+
+mongoose.connect('mongodb://localhost/react-tweets');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+  console.log('opened db conn');
+
+  var kittySchema = mongoose.Schema({
+    name: String
+	});
+
+	kittySchema.methods.speak = function () {
+	  var greeting = this.name
+	    ? "Meow name is " + this.name
+	    : "I don't have a name"
+	  console.log(greeting);
+	}
+
+	var Kitten = mongoose.model('Kitten', kittySchema)
+	
+	var silence = new Kitten({ name: 'Killer' })
+	silence.save(function (err, silence) {
+	  if (err) return console.error(err);
+	  silence.speak();
+	});
+
+	Kitten.find({ name: /^Killer/ }, function (err, kittens) {
+	  if (err) return console.error(err);
+	  console.log(kittens) // all kittens
+	})
+
+	
 });
+
+
+// Start the Server
+var server = app.listen(process.env.PORT || 3000, function(){
+	console.log('Server Loaded');
+});
+
+// Initialize socket.io
+// var io = require('socket.io').listen(server);
+
+// Create a new ntwitter instance
+// var twit = new twitter(config.twitter);
---
+++ readme.md
@@ -0,0 +1,23 @@
+install Homebrew if not already installed
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+brew doctor
+
+brew install node 
+
+npm install
+
+sudo npm install -g gulp bower
+
+gulp
+
+MongoDB
+brew install mongodb && mongod
+
+create local space for Mongo Data
+mkdir -p /data/db
+
+start mongo Server
+mongod
